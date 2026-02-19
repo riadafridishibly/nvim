942ee3ab3852ef9f4ae1bf219de4e919d9822296
Riad <riad@evatix.com>
2026-02-20 01:37:47 +0600

    Update configs

--- init.lua
+++ init.lua
@@ -476,7 +476,7 @@ require("lazy").setup({
 
 			fzflua.register_ui_select()
 
-			vim.keymap.set("", "<leader>fw", fzflua.live_grep_native)
+			vim.keymap.set("", "<leader>fw", fzflua.grep_project)
 			vim.keymap.set("", "<leader>fb", fzflua.buffers)
 			vim.keymap.set("", "<leader>f/", fzflua.lgrep_curbuf)
 			vim.keymap.set("", "<leader>fz", fzflua.resume, { desc = "Resume last fzf-lua" })
@@ -736,6 +736,7 @@ require("lazy").setup({
 	},
 	{
 		'dmtrKovalenko/fff.nvim',
+		enabled = true,
 		build = function()
 			-- this will download prebuild binary or try to use existing rustup toolchain to build from source
 			-- (if you are using lazy you can use gb for rebuilding a plugin if needed)
@@ -745,7 +746,7 @@ require("lazy").setup({
 		-- build = "nix run .#release",
 		opts = {        -- (optional)
 			debug = {
-				enabled = true, -- we expect your collaboration at least during the beta
+				enabled = false, -- we expect your collaboration at least during the beta
 				show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
 			},
 		},
@@ -763,6 +764,22 @@ require("lazy").setup({
 				function() require('fff').find_files() end,
 				desc = 'FFFind files',
 			},
+			{
+				"<leader>fg",
+				function() require('fff').live_grep() end,
+				desc = 'LiFFFe grep',
+			},
+			{
+				"<leader>fz",
+				function()
+					require('fff').live_grep({
+						grep = {
+							modes = { 'fuzzy', 'plain' }
+						}
+					})
+				end,
+				desc = 'Live fffuzy grep',
+			}
 		}
 	},
 	{
